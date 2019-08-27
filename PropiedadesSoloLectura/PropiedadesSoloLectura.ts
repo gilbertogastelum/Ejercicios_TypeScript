57c35ecb9f910772002bf8c6a4124b081b72ebcd
gilbertogastelum <47874273+gilbertogastelum@users.noreply.github.com>
2019-08-27 16:35:35 -0600

    Propiedades Solo Lectura

---
+++ PropiedadesSoloLectura/PropiedadesSoloLectura.ts
@@ -0,0 +1,17 @@
+interface enemigo{
+    readonly tamano: number,
+    vida: number,
+    rango: number,
+    readonly dano: number
+}
+
+let tanque: enemigo={
+    tamano:50,
+    vida:100,
+    rango:60,
+    dano:12
+}
+
+
+tanque.vida=95;
+//tanque.damage=10; Error, el atributo daño es de solo lectura 
