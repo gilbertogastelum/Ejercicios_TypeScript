2a991fd62a8ae050767aa3262f164bddd8bd276e
gilbertogastelum <47874273+gilbertogastelum@users.noreply.github.com>
2019-08-29 16:17:12 -0600

    Acomodo

---
+++ Ejercicios_Libro2/For-In/For_in.js
@@ -0,0 +1,9 @@
+var list = { a: 1, b: 2, c: 3 };
+for (var i in list) {
+    console.log(list[i]);
+}
+//Acceder al indice de una variavle dentro del objeto
+var list1 = { a: 1, b: 2, c: 3 };
+for (var i in list1) {
+    console.log(i);
+}
---
+++ Ejercicios_Libro2/For-In/For_in.ts
@@ -0,0 +1,12 @@
+let list ={a:1,b:2,c:3}
+
+for (let i in list){
+    console.log(list[i]);
+}
+
+//Acceder al indice de una variavle dentro del objeto
+let list1={a:1,b:2,c:3}
+
+for (let i in list1){
+    console.log(i);
+}
