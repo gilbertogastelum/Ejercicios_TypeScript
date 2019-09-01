a89cc379c2373a3a9c00ef1a9fe5d9fcad28754d
gilbertogastelum <47874273+gilbertogastelum@users.noreply.github.com>
2019-09-01 15:02:33 -0600

    Esperas Asincronas

---
+++ Ejercicios_Libro2/EsperasAsincronas/EsperasAsincronas.ts
@@ -0,0 +1,11 @@
+async function foo() {
+    try {
+        var val = await getMeAPromise();
+        console.log(val);
+    }
+    catch(err) {
+        console.log('Error: ', err.message);
+    }
+}
+//
+//
