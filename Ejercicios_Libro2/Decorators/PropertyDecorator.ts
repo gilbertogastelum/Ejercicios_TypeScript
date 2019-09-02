b6812b23ee0dc3290888c92ff64c436dc2523012
Gilberto <gilberto.gastelum.acosta@gmail.com>
2019-09-02 00:39:44 -0600

    Property Decorator

---
+++ Ejercicios_Libro2/Decorators/PropertyDecorator.ts
@@ -0,0 +1,11 @@
+function PropertyDecorator(
+    target: Object, 
+    propertyKey: string | symbol 
+    ) {
+    console.log("PropertyDecorator called on: ", target, propertyKey);
+}
+class PropertyDecoratorExample {
+    @PropertyDecorator
+    name: string;
+}
+PropertyDecorator called on: {} name
